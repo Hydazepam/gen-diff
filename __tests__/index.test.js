425b7d63c4efa5cb7bf27794411d06ef019565a7
Akadem <kostianetskyi@gmail.com>
2019-11-04 12:49:22 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,7 +5,7 @@ describe('Simple files', () => {
   test('compare two simple json config files', () => {
     const path1 = './before.json';
     const path2 = './after.json';
-    const resultPath = './__tests__/compareResult.txt';
+    const resultPath = './__tests__/__fixtures__/compareResult.txt';
 
     const diff = genDiff(path1, path2);
     const result = fs.readFileSync(resultPath, 'utf-8');

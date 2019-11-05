10db6a5eca72f7733cc1f98882c232f222df1f4e
Akadem <kostianetskyi@gmail.com>
2019-11-05 10:44:32 +0300

    added new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,4 +12,26 @@ describe('Simple files', () => {
 
     expect(diff).toEqual(result);
   });
+
+  test('compare two simple yaml config files', () => {
+    const path1 = './before.yaml';
+    const path2 = './after.yaml';
+    const resultPath = './__tests__/__fixtures__/compareResult.txt';
+
+    const diff = genDiff(path1, path2);
+    const result = fs.readFileSync(resultPath, 'utf-8');
+
+    expect(diff).toEqual(result);
+  });
+
+  test('compare two simple ini config files', () => {
+    const path1 = './before.ini';
+    const path2 = './after.ini';
+    const resultPath = './__tests__/__fixtures__/compareResult.txt';
+
+    const diff = genDiff(path1, path2);
+    const result = fs.readFileSync(resultPath, 'utf-8');
+
+    expect(diff).toEqual(result);
+  });
 });

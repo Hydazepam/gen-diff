1fc3f88d79271e68ce6d3ef7edf11002aa6a51b4
Akadem <kostianetskyi@gmail.com>
2019-11-05 11:01:28 +0300

    change tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,36 +2,14 @@ import fs from 'fs';
 import genDiff from '../src';
 
 describe('Simple files', () => {
-  test('compare two simple json config files', () => {
-    const path1 = './before.json';
-    const path2 = './after.json';
-    const resultPath = './__tests__/__fixtures__/compareResult.txt';
-
-    const diff = genDiff(path1, path2);
-    const result = fs.readFileSync(resultPath, 'utf-8');
-
-    expect(diff).toEqual(result);
-  });
-
-  test('compare two simple yaml config files', () => {
-    const path1 = './before.yaml';
-    const path2 = './after.yaml';
-    const resultPath = './__tests__/__fixtures__/compareResult.txt';
-
-    const diff = genDiff(path1, path2);
-    const result = fs.readFileSync(resultPath, 'utf-8');
-
-    expect(diff).toEqual(result);
-  });
-
-  test('compare two simple ini config files', () => {
-    const path1 = './before.ini';
-    const path2 = './after.ini';
-    const resultPath = './__tests__/__fixtures__/compareResult.txt';
-
-    const diff = genDiff(path1, path2);
-    const result = fs.readFileSync(resultPath, 'utf-8');
-
-    expect(diff).toEqual(result);
-  });
+  test.each([['./before.json', './after.json', './__tests__/__fixtures__/compareResult.txt'],
+    ['./before.yaml', './after.yaml', './__tests__/__fixtures__/compareResult.txt'],
+    ['./before.ini', './after.ini', './__tests__/__fixtures__/compareResult.txt']])(
+    'compare two simple config files',
+    (path1, path2, resultPath) => {
+      const diff = genDiff(path1, path2);
+      const result = fs.readFileSync(resultPath, 'utf-8');
+      expect(diff).toBe(result);
+    },
+  );
 });

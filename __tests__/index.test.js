fec1b7c8c0db57638ae497f399db2883a3a4236d
Akadem <kostianetskyi@gmail.com>
2019-11-08 11:22:05 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,7 +5,7 @@ describe('Simple files', () => {
   test.each([['./__tests__/__fixtures__/nestedBefore.json', './__tests__/__fixtures__/nestedAfter.json', './__tests__/__fixtures__/compareResultNested.txt'],
     ['./__tests__/__fixtures__/nestedBefore.yaml', './__tests__/__fixtures__/nestedAfter.yaml', './__tests__/__fixtures__/compareResultNested.txt'],
     ['./__tests__/__fixtures__/nestedBefore.ini', './__tests__/__fixtures__/nestedAfter.ini', './__tests__/__fixtures__/compareResultNested.txt']])(
-    'compare two nested json config files',
+    'compare two nested config files',
     (path1, path2, resultPath) => {
       const diff = genDiff(path1, path2);
       const result = fs.readFileSync(resultPath, 'utf-8');

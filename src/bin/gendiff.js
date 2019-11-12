d5190358b14c30935982308a9a64ff5826bd85dd
Akadem <kostianetskyi@gmail.com>
2019-11-12 10:13:43 +0300

    changed

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,8 +7,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
-  .action((firstConfig, secondConfig) => {
-    console.log(genDiff(firstConfig, secondConfig));
+  .action((firstConfig, secondConfig, { format }) => {
+    console.log(genDiff(firstConfig, secondConfig, format));
   });
 
 program.parse(process.argv);

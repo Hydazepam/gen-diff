7aa3d5901af668d585605c503b0b4523732807e9
Akadem <kostianetskyi@gmail.com>
2019-11-12 10:14:18 +0300

    updated

--- src/renderers.js
+++ src/renderers.js
@@ -1,35 +1,11 @@
-import _ from 'lodash';
+import getNestedDif from './formatters/nested';
+import getPlainDiff from './formatters/plain';
 
-const getIndent = (depth) => '  '.repeat(depth);
-
-const createString = (name, value, depth, sign = ' ') => {
-  if (!(value instanceof Object)) {
-    return `${getIndent(depth)}${sign} ${name}: ${value}`;
-  }
-  const children = Object.keys(value).map((key) => createString(key, value[key], depth + 2));
-  return _.flatten([
-    `${getIndent(depth)}${sign} ${name}: {`,
-    children,
-    `${getIndent(depth)}  }`,
-  ]).join('\n');
+const renders = {
+  nested: getNestedDif,
+  plain: getPlainDiff,
 };
 
-const getRenderer = (ast) => {
-  const iter = (nodes, depth) => {
-    const mapping = {
-      fixed: ({ key, children }) => createString(key, children, depth + 1),
-      updated: ({ key, valueBefore, valueAfter }) => [createString(key, valueAfter, depth + 1, '+'), createString(key, valueBefore, depth + 1, '-')],
-      deleted: ({ key, valueBefore }) => createString(key, valueBefore, depth + 1, '-'),
-      added: ({ key, valueAfter }) => createString(key, valueAfter, depth + 1, '+'),
-      nested: ({ key, children }) => [`${getIndent(depth + 1)}  ${key}: {`, iter(children, depth + 2), `${getIndent(depth + 1)}  }`],
-    };
-
-    const newNodes = nodes.map((node) => mapping[node.type](node));
-
-    return _.flatten(newNodes).join('\n');
-  };
-
-  return `{\n${iter(ast, 0)}\n}`;
-};
+const getRenderer = (format) => renders[format];
 
 export default getRenderer;

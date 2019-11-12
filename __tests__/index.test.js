08318c0a5fa584b86ccf94eb904dcd5fe3b45de2
Akadem <kostianetskyi@gmail.com>
2019-11-12 10:13:18 +0300

    added new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,7 +7,18 @@ describe('Simple files', () => {
     ['./__tests__/__fixtures__/nestedBefore.ini', './__tests__/__fixtures__/nestedAfter.ini', './__tests__/__fixtures__/compareResultNested.txt']])(
     'compare two nested config files',
     (path1, path2, resultPath) => {
-      const diff = genDiff(path1, path2);
+      const diff = genDiff(path1, path2, 'nested');
+      const result = fs.readFileSync(resultPath, 'utf-8');
+      expect(diff).toBe(result);
+    },
+  );
+
+  test.each([['./__tests__/__fixtures__/nestedBefore.json', './__tests__/__fixtures__/nestedAfter.json', './__tests__/__fixtures__/compareResultNestedPlain.txt'],
+    ['./__tests__/__fixtures__/nestedBefore.yaml', './__tests__/__fixtures__/nestedAfter.yaml', './__tests__/__fixtures__/compareResultNestedPlain.txt'],
+    ['./__tests__/__fixtures__/nestedBefore.ini', './__tests__/__fixtures__/nestedAfter.ini', './__tests__/__fixtures__/compareResultNestedPlain.txt']])(
+    'compare two nested config files with plain output',
+    (path1, path2, resultPath) => {
+      const diff = genDiff(path1, path2, 'plain');
       const result = fs.readFileSync(resultPath, 'utf-8');
       expect(diff).toBe(result);
     },

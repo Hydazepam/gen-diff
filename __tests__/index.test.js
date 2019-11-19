3eeda6dd30461c835f1e7f7fbeb6178cd93b3c58
Akadem <kostianetskyi@gmail.com>
2019-11-19 12:32:20 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,45 +1,51 @@
 import fs from 'fs';
 import genDiff from '../src';
 
+const absolutePath = `${__dirname}/__fixtures__`;
+
 describe('Simple files', () => {
-  test.each([['./__tests__/__fixtures__/nestedBefore.json', './__tests__/__fixtures__/nestedAfter.json', './__tests__/__fixtures__/compareResultNested.txt'],
-    ['./__tests__/__fixtures__/nestedBefore.yaml', './__tests__/__fixtures__/nestedAfter.yaml', './__tests__/__fixtures__/compareResultNested.txt'],
-    ['./__tests__/__fixtures__/nestedBefore.ini', './__tests__/__fixtures__/nestedAfter.ini', './__tests__/__fixtures__/compareResultNested.txt']])(
+  test.each([[`${absolutePath}/nestedBefore.json`, `${absolutePath}/nestedAfter.json`],
+    [`${absolutePath}/nestedBefore.yaml`, `${absolutePath}/nestedAfter.yaml`],
+    [`${absolutePath}/nestedBefore.ini`, `${absolutePath}/nestedAfter.ini`]])(
     'compare two nested config files',
-    (path1, path2, resultPath) => {
+    (path1, path2) => {
+      const resultPath = `${absolutePath}/compareResultNested.txt`;
       const diff = genDiff(path1, path2, 'nested');
       const result = fs.readFileSync(resultPath, 'utf-8');
       expect(diff).toBe(result);
     },
   );
 
-  test.each([['./__tests__/__fixtures__/nestedBefore.json', './__tests__/__fixtures__/nestedAfter.json', './__tests__/__fixtures__/compareResultNested.txt'],
-    ['./__tests__/__fixtures__/nestedBefore.yaml', './__tests__/__fixtures__/nestedAfter.yaml', './__tests__/__fixtures__/compareResultNested.txt'],
-    ['./__tests__/__fixtures__/nestedBefore.ini', './__tests__/__fixtures__/nestedAfter.ini', './__tests__/__fixtures__/compareResultNested.txt']])(
+  test.each([[`${absolutePath}/nestedBefore.json`, `${absolutePath}/nestedAfter.json`],
+    [`${absolutePath}/nestedBefore.yaml`, `${absolutePath}/nestedAfter.yaml`],
+    [`${absolutePath}/nestedBefore.ini`, `${absolutePath}/nestedAfter.ini`]])(
     'compare two nested config files without indicated format',
-    (path1, path2, resultPath) => {
+    (path1, path2) => {
+      const resultPath = `${absolutePath}/compareResultNested.txt`;
       const diff = genDiff(path1, path2);
       const result = fs.readFileSync(resultPath, 'utf-8');
       expect(diff).toBe(result);
     },
   );
 
-  test.each([['./__tests__/__fixtures__/nestedBefore.json', './__tests__/__fixtures__/nestedAfter.json', './__tests__/__fixtures__/compareResultNestedPlain.txt'],
-    ['./__tests__/__fixtures__/nestedBefore.yaml', './__tests__/__fixtures__/nestedAfter.yaml', './__tests__/__fixtures__/compareResultNestedPlain.txt'],
-    ['./__tests__/__fixtures__/nestedBefore.ini', './__tests__/__fixtures__/nestedAfter.ini', './__tests__/__fixtures__/compareResultNestedPlain.txt']])(
+  test.each([[`${absolutePath}/nestedBefore.json`, `${absolutePath}/nestedAfter.json`],
+    [`${absolutePath}/nestedBefore.yaml`, `${absolutePath}/nestedAfter.yaml`],
+    [`${absolutePath}/nestedBefore.ini`, `${absolutePath}/nestedAfter.ini`]])(
     'compare two nested config files with plain output',
-    (path1, path2, resultPath) => {
+    (path1, path2) => {
+      const resultPath = `${absolutePath}/compareResultNestedPlain.txt`;
       const diff = genDiff(path1, path2, 'plain');
       const result = fs.readFileSync(resultPath, 'utf-8');
       expect(diff).toBe(result);
     },
   );
 
-  test.each([['./__tests__/__fixtures__/nestedBefore.json', './__tests__/__fixtures__/nestedAfter.json', './__tests__/__fixtures__/compareResultNested.json'],
-    ['./__tests__/__fixtures__/nestedBefore.yaml', './__tests__/__fixtures__/nestedAfter.yaml', './__tests__/__fixtures__/compareResultNested.json'],
-    ['./__tests__/__fixtures__/nestedBefore.ini', './__tests__/__fixtures__/nestedAfter.ini', './__tests__/__fixtures__/compareResultNested.json']])(
+  test.each([[`${absolutePath}/nestedBefore.json`, `${absolutePath}/nestedAfter.json`],
+    [`${absolutePath}/nestedBefore.yaml`, `${absolutePath}/nestedAfter.yaml`],
+    [`${absolutePath}/nestedBefore.ini`, `${absolutePath}/nestedAfter.ini`]])(
     'compare two nested config files with JSON output',
-    (path1, path2, resultPath) => {
+    (path1, path2) => {
+      const resultPath = `${absolutePath}/compareResultNested.json`;
       const diff = genDiff(path1, path2, 'json');
       const result = fs.readFileSync(resultPath, 'utf-8');
       expect(diff).toBe(result);
